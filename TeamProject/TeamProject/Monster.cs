e28faf4b1b6fa4c7b8948e8fc7662b6eac50ff3f
rlaalsxo <alsxo124@naver.com>
2023-08-28 17:19:18 +0900

    Character

--- TeamProject/TeamProject/Monster.cs
+++ TeamProject/TeamProject/Monster.cs
@@ -15,7 +15,7 @@ namespace TeamProject
         public int def { get; }
         public string name { get; }
         public string chrd { get; }
-
+        int critical;
         public Monster(string _name, string _chrd, int _level)
         {
             level = _level;
@@ -24,6 +24,23 @@ namespace TeamProject
             atk = level * 4;
             def = level * 2;
             hp = level * 20;
+            critical = 20;
+        }
+        public void GiveDamage(Player _player, int _atk)
+        {
+            int rand = new Random().Next(1, 101);
+            if (rand <= critical)
+            {
+                _player.hp -= _atk;
+            }
+            else
+            {
+                _player.hp -= _atk - (int)(_atk * def / 15);
+            }
+            if (_player.hp <= 0)
+            {
+                Console.WriteLine($"플레이어 : {_player.name}님께서 사망하였습니다.");
+            }
         }
     }
 }
--- TeamProject/TeamProject/PlayerJop.cs
+++ TeamProject/TeamProject/PlayerJop.cs
@@ -8,6 +8,84 @@ namespace TeamProject
 {
     internal class Warrior : Player
     {
-        public Warrior(string _name, string _chrd) : base(_name, _chrd, 15, 10, 150) { }
+        public Warrior(string _name, string _chrd) : base(_name, _chrd, 15, 10, 150, 40, 15) { }
+
+        public override void Skill(Monster _mob, int _atk)
+        {
+            if(_mob.chrd == "짐승")
+            {
+                _mob.hp -= _atk * 5;
+            }
+            else
+            {
+                _mob.hp -= _atk * 3;
+            }
+            
+        }
+        public override void LevelUp()
+        {
+            maxHp += level * 50;
+            hp = maxHp;
+            maxMp += level * 10;
+            mp = maxMp;
+            atk += level * 3;
+            def += level * 3;
+            exp -= level * 100;
+            level++;
+        }
+    }
+    internal class Prist : Player
+    {
+        public Prist(string _name, string _chrd) : base(_name, _chrd, 10, 5, 100, 80, 20) { }
+
+        public override void Skill(Monster _mob, int _atk)
+        {
+            if(_mob.chrd == "언데드")
+            {
+                _mob.hp -= _atk * 2;
+            }
+            hp += _atk * 2;
+            if (hp > maxHp)
+            {
+                hp = maxHp;
+            }
+        }
+        public override void LevelUp()
+        {
+            maxHp += level * 30;
+            hp = maxHp;
+            maxMp += level * 20;
+            mp = maxMp;
+            atk += level * 5;
+            def += level * 2;
+            exp -= level * 100;
+            level++;
+        }
+    }
+    internal class Archer : Player
+    {
+        public Archer(string _name, string _chrd) : base(_name, _chrd, 20, 3, 100, 50, 40) { }
+        public override void Skill(Monster _mob, int _atk)
+        {
+            if (_mob.chrd == "악마")
+            {
+                _mob.hp -= _atk * 6;
+            }
+            else
+            {
+                _mob.hp -= _atk * 4;
+            }
+        }
+        public override void LevelUp()
+        {
+            maxHp += level * 15;
+            hp = maxHp;
+            maxMp += level * 10;
+            mp = maxMp;
+            atk += level * 7;
+            def += level * 1;
+            exp -= level * 100;
+            level++;
+        }
     }
 }
