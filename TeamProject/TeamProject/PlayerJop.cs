92bfa79490cb65b991b20449201df170e1d022d3
rlaalsxo <alsxo124@naver.com>
2023-08-28 19:34:53 +0900

    character2

--- TeamProject/TeamProject/PlayerJop.cs
+++ TeamProject/TeamProject/PlayerJop.cs
@@ -9,7 +9,12 @@ namespace TeamProject
     internal class Warrior : Player
     {
         public Warrior(string _name, string _chrd) : base(_name, _chrd, 15, 10, 150, 40, 15) { }
-
+        public override void SkillInfo()
+        {
+            Console.WriteLine("소드 스트라이크 - MP 15");
+            Console.WriteLine("공격력의 3배의 피해를 입힙니다");
+            Console.WriteLine("짐승에게는 공격력의 5배의 피해를 입힙니다");
+        }
         public override void Skill(Monster _mob, int _atk)
         {
             if(_mob.chrd == "짐승")
@@ -20,7 +25,7 @@ namespace TeamProject
             {
                 _mob.hp -= _atk * 3;
             }
-            
+            mp -= 15;
         }
         public override void LevelUp()
         {
@@ -30,14 +35,19 @@ namespace TeamProject
             mp = maxMp;
             atk += level * 3;
             def += level * 3;
-            exp -= level * 100;
+            exp -= level * 5;
             level++;
         }
     }
     internal class Prist : Player
     {
         public Prist(string _name, string _chrd) : base(_name, _chrd, 10, 5, 100, 80, 20) { }
-
+        public override void SkillInfo()
+        {
+            Console.WriteLine("홀리 엔젤 - MP 20");
+            Console.WriteLine("공격력의 2배 만큼 체력을 회복합니다");
+            Console.WriteLine("언데드에게는 추가로 공격력의 2배의 피해를 입힙니다");
+        }
         public override void Skill(Monster _mob, int _atk)
         {
             if(_mob.chrd == "언데드")
@@ -58,13 +68,19 @@ namespace TeamProject
             mp = maxMp;
             atk += level * 5;
             def += level * 2;
-            exp -= level * 100;
+            exp -= level * 5;
             level++;
         }
     }
     internal class Archer : Player
     {
         public Archer(string _name, string _chrd) : base(_name, _chrd, 20, 3, 100, 50, 40) { }
+        public override void SkillInfo()
+        {
+            Console.WriteLine("천공의 바람 - MP 15");
+            Console.WriteLine("공격력의 4배 만큼 피해를 입힙니다");
+            Console.WriteLine("악마에게는 공격력의 6배의 피해를 입힙니다");
+        }
         public override void Skill(Monster _mob, int _atk)
         {
             if (_mob.chrd == "악마")
@@ -84,7 +100,7 @@ namespace TeamProject
             mp = maxMp;
             atk += level * 7;
             def += level * 1;
-            exp -= level * 100;
+            exp -= level * 5;
             level++;
         }
     }
