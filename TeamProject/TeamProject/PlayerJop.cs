d4f12a40a047af806fad8bc80d34b1c581a16a3e
조범준 <141565207+KimMaYa1@users.noreply.github.com>
2023-08-28 14:38:20 +0900

    팀프로젝트 틀

---
+++ TeamProject/TeamProject/PlayerJop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject
+{
+    internal class PlayerJop : Player
+    {
+        public PlayerJop(string _name, string _chrd, int _atk, int _def, int _hp) : base(_name, _chrd,_atk,_def,_hp) { }
+    }
+}
