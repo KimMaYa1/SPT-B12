9606d26f5471a4263ef6d5b005ad63cbf3583195
Shyunju <hyunju9908@naver.com>
2023-08-28 19:39:16 +0900

    Item 상위 클래스

--- TeamProject/TeamProject/Item.cs
+++ TeamProject/TeamProject/Item.cs
@@ -9,11 +9,54 @@ namespace TeamProject
 {
     internal class Item
     {
-        public int eqAtk;           // 장착 무기
-        public int eaDef;           // 장착 방어력
-        public string name;         // 아이템 이름
-        public string info;         // 아이템 정보
-        public int type;            // 0 무기 1 방어구
-        public bool IsEquiped;
+
+        public int EqAtk;           // 장착 무기
+        public int EqDef;           // 장착 방어력
+        public string Name;         // 아이템 이름
+        public string Info;         // 아이템 정보
+        public int Type;            // 0 무기 1 방어구
+        public bool IsEquiped;   //착용유무
+        public int Price;           //가격
+        Player Player;
+
+
+
+        public Item(int _eqAtk, int _eqDef, int _type, int _price, string _name, string _info, Player player)
+        {
+            EqAtk = _eqAtk;
+            EqDef = _eqDef;
+            Type = _type;
+            Price = _price;
+            Name = _name;
+            Info = _info;
+            IsEquiped = false;
+            Player = player;
+
+        }
+        public virtual void Perfomence()
+        {
+            //Player.LevelUp();
+        }
+
+
+    }
+    internal class Weapon : Item
+    {
+        public Weapon(int _eqAtk, int _eqDef, int _type, int _price, string _name, string _info, Player _player) : base(_eqAtk, _eqDef, _type, _price, _name, _info, _player) { }
+
+
+    }
+    internal class Defense : Item
+    {
+        public Defense(int _eqAtk, int _eqDef, int _type, int _price, string _name, string _info, Player _player) : base(_eqAtk, _eqDef, _type, _price, _name, _info, _player) { }
+
+
+    }
+
+    internal class Potion : Item
+    {
+        public Potion(int _eqAtk, int _eqDef, int _type, int _price, string _name, string _info, Player _player) : base(_eqAtk, _eqDef, _type, _price, _name, _info, _player) { }
+
+
     }
 }
