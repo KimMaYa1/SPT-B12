f84b997754c12b2bcaf7aafec638020bd6ba0fc7
Shyunju <hyunju9908@naver.com>
2023-08-29 11:18:05 +0900

    변수명 수정

--- TeamProject/TeamProject/Item.cs
+++ TeamProject/TeamProject/Item.cs
@@ -10,27 +10,27 @@ namespace TeamProject
     internal class Item
     {
 
-        public int EqAtk;           // 장착 무기
-        public int EqDef;           // 장착 방어력
-        public string Name;         // 아이템 이름
-        public string Info;         // 아이템 정보
-        public int Type;            // 0 무기 1 방어구
-        public bool IsEquiped;   //착용유무
-        public int Price;           //가격
-        public Player Player;// { get; set; }
+        public int eqAtk;           // 장착 무기
+        public int eqDef;           // 장착 방어력
+        public string name;         // 아이템 이름
+        public string info;         // 아이템 정보
+        public int type;            // 0 무기 1 방어구
+        public bool isEquiped;   //착용유무
+        public int price;           //가격
+        public Player player;// { get; set; }
 
 
 
-        public Item(int _eqAtk, int _eqDef, int _type, int _price, string _name, string _info, Player player)
+        public Item(int _eqAtk, int _eqDef, int _type, int _price, string _name, string _info, Player _player)
         {
-            EqAtk = _eqAtk;
-            EqDef = _eqDef;
-            Type = _type;
-            Price = _price;
-            Name = _name;
-            Info = _info;
-            IsEquiped = false;
-            Player = player;
+            eqAtk = _eqAtk;
+            eqDef = _eqDef;
+            type = _type;
+            price = _price;
+            name = _name;
+            info = _info;
+            isEquiped = false;
+            player = _player;
 
         }
         public virtual void Perfomence()
@@ -43,13 +43,6 @@ namespace TeamProject
     {
         public Weapon(int _eqAtk, int _eqDef, int _type, int _price, string _name, string _info, Player _player) : base(_eqAtk, _eqDef, _type, _price, _name, _info, _player) { }
         
-        public override void Perfomence() 
-        {
-            if (IsEquiped)
-            {
-                Player.itemAtk += EqAtk;
-            }
-        }
 
     }
     internal class Defense : Item
