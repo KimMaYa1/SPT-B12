a727285ed1e705e2bc23d4a8ac2e37c406c371e8
Shyunju <hyunju9908@naver.com>
2023-08-29 16:34:54 +0900

    상점 구매 판매 함수

--- TeamProject/TeamProject/Shop.cs
+++ TeamProject/TeamProject/Shop.cs
@@ -13,15 +13,13 @@ namespace TeamProject
         public static void BuyItem(int num , Player player)
         {
             player.Gold -= items[num-1].Price;
-
-            Console.WriteLine("아이템을 구매하였습니다.");
+            player.ItemAdd(items[num-1]);
         }
 
         public static void SellItem(int num, Player player)
         {
             player.Gold += player.Inventory[num-1].Price / 2;    // 해당 아이템의 구매가격의 절반으로 돌려받음
-            player.Inventory = player.Inventory.Where(idx => idx != player.Inventory[num-1]).ToArray();       //인벤토리에서 해당 아티엠을 삭제
-            Console.WriteLine("아이템을 판매하였습니다.");
+            player.ItemDelete(items[num - 1]);      //인벤토리에서 해당 아티엠을 삭제
 
         }
     }
