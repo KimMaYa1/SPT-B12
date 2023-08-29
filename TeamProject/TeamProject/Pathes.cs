43fb12f9ba5946ed3a0828523bac00d728da0416
francJi <minkyuji95@hanmail.net>
2023-08-29 15:30:27 +0900

    Pathes.cs 변경

--- TeamProject/TeamProject/Pathes.cs
+++ TeamProject/TeamProject/Pathes.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace TeamProject
 {
-    public class Pathes
+    public static class Pathes
     {
+        public static string localPath = Directory.GetParent(Path.GetFullPath(@"..\Data\MonsterData.csv")).Parent.Parent.Parent.Parent.ToString();
+        public static string MonsterDataPath()
+        {
+            var dataPath = @"Data";
+            var fileName = @"MonsterData.csv";
+            var fullPath = Path.Combine(localPath, dataPath, fileName);
+            return fullPath;
+        }
+
+        public static string ItemDataPath()
+        {
+            var dataPath = @"Data";
+            var fileName = @"ItemData.csv";
+            var fullPath = Path.Combine(localPath, dataPath, fileName);
+            return fullPath;
+        }
     }
 }
