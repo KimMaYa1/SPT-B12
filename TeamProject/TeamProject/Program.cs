43789c9fe9abd9c26249d7674dbb20723649b5fd
조범준 <141565207+KimMaYa1@users.noreply.github.com>
2023-08-29 16:34:29 +0900

    씬화면커밋

--- TeamProject/TeamProject/Program.cs
+++ TeamProject/TeamProject/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Scene scene = new Scene();
+            bool isSelectNameChrd = true;
+            while (isSelectNameChrd)
+            {
+                isSelectNameChrd = scene.DisplaySelectName();
+            }
+
             while (true)
             {
                 scene.DisplayStart();
--- TeamProject/TeamProject/Scene.cs
+++ TeamProject/TeamProject/Scene.cs
@@ -14,6 +14,7 @@ namespace TeamProject
         Monster[] _monsters;
         int _beforeHp = 0;
         int _stage = 1;
+        int _round = 1;
 
         public int InputString(int min, int max, int num, string str)
         {
@@ -183,48 +184,6 @@ namespace TeamProject
             return true;
         }
 
-        public void DisplayStart()
-        {
-            Console.Clear();
-            Console.WriteLine();
-            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
-            Console.WriteLine("이제 전투를 시작할 수 있습니다.");
-            Console.WriteLine();
-            Console.WriteLine("1. 상태 보기");
-            Console.WriteLine("2. 전투 시작 - 현재 스테이지 {0}", _stage);
-            Console.WriteLine();
-
-            int inputNum = InputString(1, 2, 0, "원하시는 행동을 입력해주세요.");
-
-            Console.WriteLine();
-
-            bool isStat = true;
-            if (inputNum == 1)
-            {
-                Console.WriteLine("===========================");
-                Console.WriteLine("  상태 보기 창으로 이동중");
-                Console.WriteLine("===========================");
-                Thread.Sleep(1000);
-
-                while (isStat)
-                {
-                    isStat = DisplayStat();
-                }
-            }
-            else if (inputNum == 2)
-            {
-                Console.WriteLine("=====================");
-                Console.WriteLine("  전투창으로 이동중");
-                Console.WriteLine("=====================");
-                Thread.Sleep(1000);
-
-                while (isStat)
-                {
-                    isStat = DisplayBattle();
-                }
-            }
-        }
-
         public bool DisplayStat()
         {
             Console.Clear();
@@ -260,13 +219,114 @@ namespace TeamProject
             return true;
         }
 
-        public bool DisplayBattle()
+        public void DisplayStart()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine("이제 전투를 시작할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("1. 상태 보기");
+            Console.Write("2. 전투 시작 - 현재 스테이지 {0}-", _stage);
+            if (_round == 4)
+            {
+                Console.WriteLine("보스");
+            }
+            else
+            {
+                Console.WriteLine("{0}", _round);
+            }
+            Console.WriteLine();
+
+            int inputNum = InputString(1, 2, 0, "원하시는 행동을 입력해주세요.");
+
+            Console.WriteLine();
+
+            bool isStat = true;
+            if (inputNum == 1)
+            {
+                Console.WriteLine("===========================");
+                Console.WriteLine("  상태 보기 창으로 이동중");
+                Console.WriteLine("===========================");
+                Thread.Sleep(1000);
+
+                while (isStat)
+                {
+                    isStat = DisplayStat();
+                }
+            }
+            else if (inputNum == 2)
+            {
+                int input = _round;
+                if(_round == 4)
+                {
+                    Console.WriteLine("============================");
+                    Console.WriteLine("  라운드 선택창으로 이동중");
+                    Console.WriteLine("============================");
+                    Thread.Sleep(1000);
+
+                    input = DisplaySelectRound();
+                }
+
+                if (input == 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("=====================");
+                    Console.WriteLine("  시작창으로 이동중");
+                    Console.WriteLine("=====================");
+                    Thread.Sleep(1000);
+                    return;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("=====================");
+                Console.WriteLine("  전투창으로 이동중");
+                Console.WriteLine("=====================");
+                Thread.Sleep(1000);
+
+                _dungeon = new Dungeon(_stage, input);
+                _monsters = _dungeon.MonsterGen();
+
+                while (isStat)
+                {
+                    isStat = DisplayBattle(input);
+                }
+
+            }
+        }
+
+        public int DisplaySelectRound()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("라운드 선택");
+            Console.WriteLine("원하시는 라운드로 입장하실수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("1. {0}-1",_stage);
+            Console.WriteLine("2. {0}-2",_stage);
+            Console.WriteLine("3. {0}-3",_stage);
+            Console.WriteLine("4. {0}-보스",_stage);
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine();
+
+            int input = InputString(0, 4, 0, "원하시는 던전을 입력해주세요.");
+
+            if (input == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return input;
+            }
+        }
+
+        public bool DisplayBattle(int round)
         {
             _beforeHp = _player.Hp;
-            _dungeon = new Dungeon(_stage);
-            _monsters = _dungeon.MonsterGen();
 
-            BattleInfo(false);
+            BattleInfo(false, round);
 
             Console.WriteLine("1. 공격");
             Console.WriteLine();
@@ -278,17 +338,20 @@ namespace TeamProject
             {
                 while (isAttack)
                 {
-                    isAttack = DisplayAttackSelect();
+                    isAttack = DisplayAttackSelect(round);
                 }
-                _stage++;
+                if (_player.Hp >= 0 && !IsDeadMonsters())
+                {
+                    return true;
+                }
                 return false;
             }
             return true;
         }
 
-        public bool DisplayAttackSelect()
+        public bool DisplayAttackSelect(int round)
         {
-            BattleInfo(true);
+            BattleInfo(true, round);
 
             Console.WriteLine("0. 취소");
             Console.WriteLine();
@@ -321,18 +384,18 @@ namespace TeamProject
 
             if (_player.Hp <= 0 || IsDeadMonsters())
             {
-                DisplayBattleClear();
+                DisplayBattleClear(round);
                 return false;
             }
 
             return true;
         }
 
-        public void BattleInfo(bool isBattle)
+        public void BattleInfo(bool isBattle, int round)
         {
             Console.Clear();
             Console.WriteLine();
-            Console.WriteLine("Battle!! - stage {0}", _stage);
+            Console.WriteLine("Battle!! - 현재 스테이지 {0}-{1}", _stage, round);
             Console.WriteLine();
             Console.WriteLine("[몬스터 정보]");
             int j = 4;
@@ -372,12 +435,13 @@ namespace TeamProject
             Console.WriteLine();
             Console.WriteLine("[내 정보]");
             Console.WriteLine("레벨 | {0}\tChrd | {1}", _player.Level, _player.Chrd);
-            Console.WriteLine("체력 | {0}/100", _player.Hp);
+            Console.WriteLine("체력 | {0}/{1}", _player.Hp, _player.MaxHp);
             Console.WriteLine();
         }
 
         public void AttackInfo(Character aCharacter, Character tCharacter)
         {
+            Console.WriteLine();
             if (tCharacter.Evasion())
             {
                 Console.WriteLine("========================================");
@@ -387,10 +451,10 @@ namespace TeamProject
             else
             {
                 int damage = tCharacter.TakeDamage(aCharacter.Atk);
-                Console.WriteLine();
                 Console.WriteLine("==============================================");
                 Console.WriteLine(" {0}이(가) {1}에게 {2}의 데미지를 입혔습니다", aCharacter.Name, tCharacter.Name, damage);
                 Console.WriteLine("==============================================");
+                Console.WriteLine();
                 if (tCharacter.Hp < damage)
                 {
                     Console.WriteLine("{0}의 체력 {1} -> 0", tCharacter.Name, tCharacter.Hp);
@@ -418,7 +482,7 @@ namespace TeamProject
             return true;
         }
 
-        public void DisplayBattleClear()
+        public void DisplayBattleClear(int round)
         {
             Console.Clear();
             Console.WriteLine();
@@ -452,6 +516,18 @@ namespace TeamProject
                 Console.WriteLine("경험치 | {0} / {1} -> {2} / {3}\t(+{4})", beforeExp, beforeMaxExp, _player.Exp, _player.Level * 5, exp);
                 Console.WriteLine();
                 Console.WriteLine("체력   | {0} / {1} -> {2} / {1}", _beforeHp, _player.MaxHp, _player.Hp);
+                if (_round == 4)
+                {
+                    if (round == 4)
+                    {
+                        _stage++;
+                        _round = 1;
+                    }
+                }
+                else
+                {
+                    _round++;
+                }
             }
             else
             {
