42e5aa3e0f2df0b4e4e544c9ce4849d3a1cee6c7
Shyunju <hyunju9908@naver.com>
2023-08-31 15:58:29 +0900

    아이템 팔기 수정

--- TeamProject/TeamProject/Shop.cs
+++ TeamProject/TeamProject/Shop.cs
@@ -133,7 +133,7 @@ namespace TeamProject
         public static void SellItem(int num, Player player)
         {
             player.Gold += player.Inventory[num-1].Price / 2;    // 해당 아이템의 구매가격의 절반으로 돌려받음
-            player.ItemDelete(items[num - 1]);      //인벤토리에서 해당 아티엠을 삭제
+            player.ItemDelete(player.Inventory[num - 1]);      //인벤토리에서 해당 아티엠을 삭제
 
         }
 
