adcd6c1138dfa1d5eb9eb3f8c6523d2df7da62f8
rlaalsxo <alsxo124@naver.com>
2023-09-01 10:48:01 +0900

    Revert "Update Program.cs"
    This reverts commit e9f006d353e584820177cf3249c56d7952b68e23.

--- TeamProject/TeamProject/Program.cs
+++ TeamProject/TeamProject/Program.cs
@@ -7,7 +7,7 @@
             Scene scene = new Scene();
             bool isSelectNameChrd = false;
             Console.SetWindowSize(161, 41);
-            if (DataSave.HowChoose(scene))
+            /*if (DataSave.HowChoose(scene))
             {
                 scene._player = DataSave.LoadPlayer();
                 scene._stage = scene._player.Stage;
@@ -16,7 +16,10 @@
             else
             {
                 isSelectNameChrd = true;
-            }
+            }*/
+            
+            isSelectNameChrd = true;
+
             while (isSelectNameChrd)
             {
                 isSelectNameChrd = scene.DisplaySelectName();
